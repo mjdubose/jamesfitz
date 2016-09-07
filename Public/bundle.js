4024eb8744dc32ed00bbd9cc90db68eb8df53567
James <Fitz.programk@gmail.com>
2016-09-07 10:22:39 -0500

    adds music

--- Public/bundle.js
+++ Public/bundle.js
@@ -19769,6 +19769,10 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	var _audio = __webpack_require__(161);
+
+	var _audio2 = _interopRequireDefault(_audio);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19791,23 +19795,24 @@
 			value: function render() {
 
 				return _react2.default.createElement(
-					'form',
+					'div',
 					null,
 					_react2.default.createElement(
-						'label',
-						{ className: 'battle-tag-header' },
-						'Enter Battle Tag'
+						'form',
+						null,
+						_react2.default.createElement(
+							'label',
+							{ className: 'battle-tag-header' },
+							'Enter Battle Tag'
+						),
+						_react2.default.createElement('input', { placeholder: 'BattleTag: example "name.1234"', type: 'text', className: 'tag-name' }),
+						_react2.default.createElement(
+							'button',
+							{ type: 'submit' },
+							'Request the hero'
+						)
 					),
-					_react2.default.createElement(
-						'input',
-						{ placeholder: 'BattleTag', type: 'text' },
-						' '
-					),
-					_react2.default.createElement(
-						'button',
-						{ type: 'submit' },
-						'Request the hero'
-					)
+					_react2.default.createElement(_audio2.default, null)
 				);
 			}
 		}]);
@@ -19816,6 +19821,59 @@
 	}(_react2.default.Component);
 
 	exports.default = BattleTag;
+
+/***/ },
+/* 161 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Audio = function (_React$Component) {
+		_inherits(Audio, _React$Component);
+
+		function Audio() {
+			_classCallCheck(this, Audio);
+
+			return _possibleConstructorReturn(this, (Audio.__proto__ || Object.getPrototypeOf(Audio)).apply(this, arguments));
+		}
+
+		_createClass(Audio, [{
+			key: 'render',
+			value: function render() {
+				return _react2.default.createElement(
+					'div',
+					null,
+					_react2.default.createElement(
+						'audio',
+						{ control: true, className: 'audio', autoPlay: this.props.autoplay },
+						"<source src='/Public/20-heaven-s-gate.mp3' type='audio/mp3'>"
+					)
+				);
+			}
+		}]);
+
+		return Audio;
+	}(_react2.default.Component);
+
+	exports.default = Audio;
 
 /***/ }
 /******/ ]);
